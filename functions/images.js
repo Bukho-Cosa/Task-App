89fa1fe13f06bf19557e9ad41a3c23cbeacbb98d
Bukho-Cosa <108462861+Bukho-Cosa@users.noreply.github.com>
2022-09-20 18:03:34 +0200

    Update 8
    ?

--- functions/images.js
+++ functions/images.js
@@ -1,4 +1,12 @@
 const image = document.getElementById("dawn");
+const imageOne = document.getElementById("dawnOne");
+
+
+const newImage = document.createElement('img');
+newImage.innerHTML = '<img src="../images/day/day 1.jpg" width="300kb">';
+
+const newImageOne = document.createElement('img');
+newImageOne.innerHTML = '<img src="../images/day/day 2.jpg" width="300kb">';
 
 const d = new Date();
 let hour = d.getHours();
@@ -20,7 +28,9 @@ switch(hour){
     case 11:
         console.log("good morning");
         document.body.style.backgroundColor = "lightblue";
-        image.style.visibility = "hidden"; /* check your flex and placements in general for css and try to change visibility on html first and then do the styling here ir do it on css and then change it here*/ 
+        image.style.visibility = "visible";
+        imageOne.style.visibility = "visible"; 
+        /* check your flex and placements in general for css and try to change visibility on html first and then do the styling here ir do it on css and then change it here*/ 
         break;
     case 12:
     case 13:
@@ -29,8 +39,18 @@ switch(hour){
     case 16:
         console.log("good afternoon");
         document.body.style.backgroundColor = "#46BBFB";
+        image.parentNode.replaceChild(newImage, image);
+        newImage.style.visibility = "visible";
+
+        imageOne.parentNode.replaceChild(newImageOne, imageOne);
+        newImageOne.style.visibility = "visible";
         break;
     default:
         console.log("good evening");
         document.body.style.backgroundColor = "#012031";
 }
+
+/**
+ * do this so your pictures aren't chunky:
+ * e.g listItem.parentNode.replaceChild(newItem, listItem);
+ */
